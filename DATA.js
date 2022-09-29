2096d7084c4452641b9c0cbbc0c56ddff9f0ca1f
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 07:00:02 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "4386060b-0a22-41b7-8500-946018fd9258",
+    "home_feed": [
+        {
+            "id": "1713afbb-74e2-45f3-a7e7-e374f782d996",
+            "artist_name": "down",
+            "home_carousel": [
+                {
+                    "id": "10c43132-8820-44a3-baa5-678b5a63275c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+                },
+                {
+                    "id": "c158a48b-958f-421a-82c0-2473d5978462",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png"
+                },
+                {
+                    "id": "95bfc6d9-9a17-4c89-9547-1903e0162ba8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                },
+                {
+                    "id": "e3fae268-79d4-4923-ae52-78da0886b454",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/111.png"
+                },
+                {
+                    "id": "9a194c53-a29c-4712-ba7c-cb5652c2c521",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png"
+                }
+            ],
+            "art_caption": "soldier",
+            "art_description": "Teacher between box magazine. Performance maintain student while garden memory. Born often morning may."
+        },
+        {
+            "id": "2b562a96-92ea-4445-b9e3-4a6a3a6ff334",
+            "artist_name": "by",
+            "home_carousel": [
+                {
+                    "id": "41c5fa4d-2c12-445c-87c4-ae60aaa1403b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/19.png"
+                },
+                {
+                    "id": "48d0bd60-0d22-4aa1-860a-cfa5c5cc314e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/323.png"
+                },
+                {
+                    "id": "cdacd33c-79d3-46d7-8f52-0caa82932f46",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png"
+                },
+                {
+                    "id": "0ecc7ddf-af08-495f-8260-c725af1504b3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                },
+                {
+                    "id": "42526a96-6999-4d55-aedb-ea4c0d2152b0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/219.png"
+                }
+            ],
+            "art_caption": "land",
+            "art_description": "That dinner such enjoy day. Increase source finally dark. Five option assume follow bag data."
+        },
+        {
+            "id": "fe7fc0f1-850b-4cb3-910c-48fd27bf699a",
+            "artist_name": "pretty",
+            "home_carousel": [
+                {
+                    "id": "f4c83877-13dd-49c5-834b-897e55ff7f6e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/214.png"
+                },
+                {
+                    "id": "793e2f7d-a1df-485f-b702-a8586b868182",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                },
+                {
+                    "id": "bbb3b169-1339-4585-92a9-9a1d08c45b18",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/313.png"
+                },
+                {
+                    "id": "cefcca6e-1fa6-4c2a-9785-64659859a17a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png"
+                },
+                {
+                    "id": "2f8b9164-1143-47ed-88f6-edab15a371d3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png"
+                }
+            ],
+            "art_caption": "hotel",
+            "art_description": "Home board region join some. Democratic serious now."
+        },
+        {
+            "id": "ea6f9faf-fd94-4ef0-80fe-6ccea468efbd",
+            "artist_name": "language",
+            "home_carousel": [
+                {
+                    "id": "4fbb6550-c86d-4113-a08e-b04fa357f122",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/278.png"
+                },
+                {
+                    "id": "bc1b6b57-5bf5-41ed-8ba7-dffca090da38",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+                },
+                {
+                    "id": "0bde059d-6805-40b7-a5db-2537426f9f6d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "6595f908-aec8-416a-b44a-9bd463415e15",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/208.png"
+                },
+                {
+                    "id": "13f03f6d-1f34-4a79-90e7-d8599cd2566e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/0.png"
+                }
+            ],
+            "art_caption": "open",
+            "art_description": "Painting actually raise six south claim mean. Small determine must. Everybody everything management dog agree far focus."
+        },
+        {
+            "id": "93dac018-d17c-4777-90ff-41f398566167",
+            "artist_name": "politics",
+            "home_carousel": [
+                {
+                    "id": "83ca249a-dba7-46fb-8b29-e95e234d9002",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/111.png"
+                },
+                {
+                    "id": "4f383b6f-19b2-430d-8356-01beb46d4e57",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/106.png"
+                },
+                {
+                    "id": "1be8f193-19ce-42a6-a391-18f15a175820",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/129.png"
+                },
+                {
+                    "id": "e3577f2a-3efd-45f7-8c42-c001734b863d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/7.png"
+                },
+                {
+                    "id": "5cb7b257-82e2-4df8-b417-c6a4c32b43dd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png"
+                }
+            ],
+            "art_caption": "want",
+            "art_description": "Or on argue."
+        }
+    ]
+}
